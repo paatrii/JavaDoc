3e1825857d438d969ea41602b75238e9759b3291
Patri <paatrii@bitbucket.org>
2019-12-18 21:34:30 +0100

    Segundo commit: añadimos algunos parametros a la clase Logica

--- src/net/aplicacion/Logica.java
+++ src/net/aplicacion/Logica.java
@@ -2,7 +2,18 @@ package net.aplicacion;
 
 import java.util.Scanner;
 
+/**
+ * Esta clase define unos metodos basicos 
+ * de logica matematica
+ * @author patri
+ *
+ */
+
 public class Logica {
+	
+	/**
+	 * metodo que permite sumar dos numeros enteros 
+	 */
 	
 	public void sumar() {
 		
@@ -12,6 +23,11 @@ public class Logica {
 		System.out.println((num1+num2));
 
 	}
+	/**
+	 * este metodo nos permite restar dos numeros enteros
+	 * @param num1 valor ingresado por el usuario
+	 * @param num2 valor ingresado por el usuario
+	 */
 	
 	public void restar(int num1, int num2) {
 		
